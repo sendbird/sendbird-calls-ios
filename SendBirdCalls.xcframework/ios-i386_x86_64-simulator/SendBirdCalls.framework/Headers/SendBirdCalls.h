4612bb9a37792253950501fc7b5551f3e863450e
Minhyuk Kim <kimminhyuk1004@gmail.com>
2020-10-21 21:31:48 +0900

    Initialize SPM

---
+++ SendBirdCalls.xcframework/ios-i386_x86_64-simulator/SendBirdCalls.framework/Headers/SendBirdCalls.h
@@ -0,0 +1,12 @@
+//
+//  SendBirdCalls.h
+//  SendBirdCalls
+//
+//  Created by sendbird-young on 2019/12/01.
+//  Copyright © 2019 SendBird. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
