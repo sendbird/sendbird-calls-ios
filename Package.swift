8cd2be0f973e684fadf237fcc4037f605bdf8c82
Minhyuk Kim <kimminhyuk1004@gmail.com>
2020-10-26 17:55:29 +0900

    Add dependency

--- Package.swift
+++ Package.swift
@@ -8,15 +8,16 @@ let package = Package(
     platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "SendBirdCall", targets: ["SendBirdCall"]),
+        .library(name: "SendBirdCalls", targets: ["SendBirdCalls"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../sendbird-webrtc-ios")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .binaryTarget(name: "SendBirdCall", path: "SendBirdCalls.xcframework")
+        .binaryTarget(name: "SendBirdCalls", path: "SendBirdCalls.xcframework")
     ]
 )
